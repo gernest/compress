5208972a06c7ef016f954e843739bb06ca6e6ed7
gernest <geofreyernest@live.com>
2018-11-12 22:38:32 +0300

    initial commit

---
+++ src/flate/flate.zig
@@ -0,0 +1,91 @@
+// 2 bits:   type   0 = literal  1=EOF  2=Match   3=Unused
+// 8 bits:   xlength = length - MIN_MATCH_LENGTH
+// 22 bits   xoffset = offset - MIN_OFFSET_SIZE, or literal
+const length_shift = 22;
+const offset_mask = 1 << length_shift - 1;
+const type_mask = 3 << 30;
+const literal_type = 0 << 30;
+const match_type = 1 << 30;
+
+// The length code for length X (MIN_MATCH_LENGTH <= X <= MAX_MATCH_LENGTH)
+// is lengthCodes[length - MIN_MATCH_LENGTH]
+const length_codes = []u32.{
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 8,
+    9, 9, 10, 10, 11, 11, 12, 12, 12, 12,
+    13, 13, 13, 13, 14, 14, 14, 14, 15, 15,
+    15, 15, 16, 16, 16, 16, 16, 16, 16, 16,
+    17, 17, 17, 17, 17, 17, 17, 17, 18, 18,
+    18, 18, 18, 18, 18, 18, 19, 19, 19, 19,
+    19, 19, 19, 19, 20, 20, 20, 20, 20, 20,
+    20, 20, 20, 20, 20, 20, 20, 20, 20, 20,
+    21, 21, 21, 21, 21, 21, 21, 21, 21, 21,
+    21, 21, 21, 21, 21, 21, 22, 22, 22, 22,
+    22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
+    22, 22, 23, 23, 23, 23, 23, 23, 23, 23,
+    23, 23, 23, 23, 23, 23, 23, 23, 24, 24,
+    24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+    24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+    24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+    25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+    25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+    25, 25, 25, 25, 25, 25, 25, 25, 25, 25,
+    25, 25, 26, 26, 26, 26, 26, 26, 26, 26,
+    26, 26, 26, 26, 26, 26, 26, 26, 26, 26,
+    26, 26, 26, 26, 26, 26, 26, 26, 26, 26,
+    26, 26, 26, 26, 27, 27, 27, 27, 27, 27,
+    27, 27, 27, 27, 27, 27, 27, 27, 27, 27,
+    27, 27, 27, 27, 27, 27, 27, 27, 27, 27,
+    27, 27, 27, 27, 27, 28,
+};
+
+const offset_codes = []u32.{
+    0, 1, 2, 3, 4, 4, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7,
+    8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9, 9,
+    10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+    11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11,
+    12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+    12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12,
+    13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
+    13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
+    14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
+    14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
+    14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
+    14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+    15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+};
+
+const token = struct.{
+    value: u32,
+    fn literalToken(value: u32) token {
+        return token.{ .value = literal_type + value };
+    }
+    fn matchToken(xlength: u32, xoffset: u32) token {
+        return token.{ .value = match_type + xlength << length_shift + xoffset };
+    }
+    fn literal(self: token) u32 {
+        return self.value - literal_type;
+    }
+    fn offset(self: token) u32 {
+        return self.value & offset_mask;
+    }
+    fn length(self: token) u32 {
+        return (self.value - match_type) >> length_shift;
+    }
+};
+
+fn lengthCode(v: u32) u32 {
+    return length_codes[@intCast(usize, v)];
+}
+
+fn offsetCode(off: u32) u32 {
+    if (@intCast(usize, off) < offset_codes.len) {
+        return offset_codes[@intCast(usize, off)];
+    }
+    if (@intCast(usize, off >> 7) < offset_codes.len) {
+        return offset_codes[@intCast(usize, off >> 7)];
+    }
+    return offset_codes[@intCast(usize, off >> 14)] + 28;
+}
